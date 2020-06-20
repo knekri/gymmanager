2791ee57134cfe8c55ae985defff370eee4d8586
Krisztian Knehr <knehr.krisztian@gmail.com>
2020-06-21 01:59:06 +0200

    add gym manager related resource, add liquibase-maven-plugin, generate changelog from existing db

---
+++ src/main/resources/dummydata/dummy_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO gym_manager.room (room_building, room_number, room_size) VALUES ('A', '001', 12);
+INSERT INTO gym_manager.room (room_building, room_number, room_size) VALUES ('A', '002', 10);
+INSERT INTO gym_manager.room (room_building, room_number, room_size) VALUES ('B', '010', 15);
+
+INSERT INTO gym_manager.user (user_name, password, first_name, last_name, birth_date, email, phone_number) 
+VALUES ('lacika', '12345', 'szabo', 'laszlo', '1991-01-02', 'szabolacika@gmail.com', '+36301234567');
+INSERT INTO gym_manager.user (user_name, password, first_name, last_name, birth_date, email, phone_number) 
+VALUES ('gaborka', '54321', 'toth', 'gabor', '1991-01-02', 'tothgaborka@gmail.com', '+36301234568');
+INSERT INTO gym_manager.user (user_name, password, first_name, last_name, birth_date, email, phone_number) 
+VALUES ('pistike', 'jelszo123', 'horvath', 'istvan', '1991-01-02', 'horvathpistike@gmail.com', '+36301234569');
+
